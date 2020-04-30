e9109671edf62239c566d731ed47d9ea8b3a3830
ElvitaKehler <mekdy.20@gmail.com>
2020-04-30 10:25:23 -0300

    se crean carpetas, htaccess y comienzo de router

---
+++ router.php
@@ -0,0 +1,28 @@
+<?php
+    //require_once 'controllers/prod.controller.php';
+
+    // definimos la base url de forma dinamica
+    define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+    // define una acción por defecto
+    if (empty($_GET['action'])) {
+        $_GET['action'] = 'listar';
+    } 
+
+    // toma la acción que viene del usuario y parsea los parámetros
+    $accion = $_GET['action']; 
+    $parametros = explode('/', $accion);
+    //var_dump($parametros); die; // like console.log();
+
+    // decide que camino tomar según TABLA DE RUTEO
+    switch ($parametros[0]) {
+        case 'listar': // /listar   ->   showProd()
+            // instanciando un objeto de la clase ProdController
+            $controller = new ProdController();
+            $controller->showProducts();
+        break;
+
+         default: 
+            echo "404 not found";
+        break;
+    }
