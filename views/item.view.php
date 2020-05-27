ffeabc2bf48a914ccf8f2070c912e4b41477b5fe
Sergio Yañez <sergiomyanez02@gmail.com>
2020-05-26 21:59:13 -0300

    Se agregó folder código a testear

--- views/item.view.php
+++ views/item.view.php
@@ -9,7 +9,7 @@ class ItemView{
         $this->smarty = new Smarty();
         $this->smarty->assign("base_url", BASE_URL);
         $this->smarty->assign("esadmin",AuthHelper::userLogged());
-    
+        $this->smarty->assign("usuario",AuthHelper::userName());
     }
 
     public function items($rubros){
