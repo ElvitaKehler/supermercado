5fe37670fd614a0b99c19fccc445cbec77de2eb7
ElvitaKehler <mekdy.20@gmail.com>
2020-06-03 18:37:55 -0300

    se agrega codigo para la vista json en API

--- API/Prod.Api.Controller.php
+++ API/Prod.Api.Controller.php
@@ -1,15 +1,19 @@
 <?
 require_once 'models/product.model.php';
+require_once 'API/APi.View.php';
 
 class ProdApiController{
     private $model;
+    private $view;
 
     function __construct(){
         $this->model = new ProductModel();
+        $this->view = new ApiView();
     }
     
     public function getprod(){
-        echo 'llega a getprod';
+        $tareas = $this->model->getAll();
+        $this->view->response($tareas,200);
 
     }
 
